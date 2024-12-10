d3d025257c62c7bc3cee3b083965a56740c2db28
Katrine Boedkergaard Nielsen <katrine.boedkergaard@gmail.com>
2024-12-10 16:21:44 +0100

    Exercise 6.5: Convert summary statistics code into a function

---
+++ R/functions.R
@@ -0,0 +1,15 @@
+descriptive_stats <- function(data) {
+    data |>
+        dplyr::group_by(metabolite) |>
+        dplyr::summarize(dplyr::across(
+            value,
+            list(
+                mean = mean,
+                sd = sd
+            )
+        )) |>
+        dplyr::mutate(dplyr::across( # across: Given these columns (.x is a placeholder for the column), do this action
+            where(is.numeric),
+            ~ round(.x, digits = 1)
+        ))
+}
